b0e0ba7cc57e3b76090277fab857dc03915021ca
Allegra Perazzo <allegra.perazzo@googlemail.com>
2017-05-09 16:46:26 +0100

    enclosure spec set up

---
+++ specs/enclosure_spec.js
@@ -0,0 +1,13 @@
+var assert = require("assert");
+var Enclosure = require("../enclosure.js");
+var Dinosaur = require("../dinosaur.js");
+
+describe("Enclosure", function(){
+
+  beforeEach(function(){
+    this.enclosure = new Enclosure("Big one");
+  })
+
+
+
+})
